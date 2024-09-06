2a0d9e9c47ea186073b42d6873b65fbf22e00e4b
Chris Cole <whitewaterdesign>
2024-09-06 20:37:06 +0100

    feat: 430523 - Add AccessToken to JWT

---
+++ app/auth/jwt-utils.js
@@ -0,0 +1,51 @@
+const jwt = require('jsonwebtoken')
+const config = require('../config')
+const permissions = require('./permissions')
+
+const generateToken = (payload, { audience, issuer }) => {
+  const privateKey = atob(config.authConfig.privateKey)
+
+  const options = {
+    expiresIn: '1h',
+    algorithm: 'RS256',
+    audience,
+    issuer,
+    keyid: issuer
+  }
+
+  return jwt.sign(payload, privateKey, options)
+}
+
+const createJwtToken = (audience) => (username, displayname, scopes, token) => {
+  const options = {
+    audience,
+    issuer: 'aphw-ddi-enforcement'
+  }
+
+  return generateToken({
+    scopes,
+    username,
+    displayname,
+    token
+  }, options)
+}
+
+const createBearerHeader = (audience) => (user) => {
+  console.log('~~~~~~ Chris Debug ~~~~~~ ', 'User', user)
+  const username = user?.username
+  const displayname = user?.displayname
+  const token = user?.accessToken
+  const scopes = [permissions.enforcement]
+
+  const bearerToken = createJwtToken(audience)(username, displayname, scopes, token)
+
+  return {
+    Authorization: `Bearer ${bearerToken}`
+  }
+}
+
+module.exports = {
+  generateToken,
+  createJwtToken,
+  createBearerHeader
+}
