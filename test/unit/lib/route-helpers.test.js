7a7e669c33d27ed20b31578bfb986f1bdb025225
work <jeremy.barnsley@esynergy.co.uk>
2024-09-24 12:46:39 +0100

    Extra test

--- test/unit/lib/route-helpers.test.js
+++ test/unit/lib/route-helpers.test.js
@@ -1,4 +1,16 @@
-const { throwIfPreConditionError } = require('../../../app/lib/route-helpers')
+const { throwIfPreConditionError, licenceNotYetAccepted } = require('../../../app/lib/route-helpers')
+
+jest.mock('../../../app/session/session-wrapper')
+const { getFromSession, setInSession } = require('../../../app/session/session-wrapper')
+
+jest.mock('../../../app/api/ddi-index-api/user')
+const { validateLicence } = require('../../../app/api/ddi-index-api/user')
+
+beforeEach(() => {
+  jest.clearAllMocks()
+  setInSession.mockReturnValue()
+})
+
 describe('throwIfPreConditionError', () => {
   test('should throw an error if any of the pre conditions throw an error', () => {
     const request = {
@@ -21,5 +33,24 @@ describe('throwIfPreConditionError', () => {
   test('should not throw an error if no pre exists', () => {
     const request = {}
     expect(() => throwIfPreConditionError(request)).not.toThrow()
+  })
+})
+
+describe('licenceNotYetAccepted', () => {
+  test('should return false if already accepted in session', async () => {
+    getFromSession.mockReturnValue('Y')
+    expect(await licenceNotYetAccepted({}, {})).toBeFalsy()
+  })
+
+  test('should return false if not accepted in session but accepted in DB', async () => {
+    getFromSession.mockReturnValue()
+    validateLicence.mockResolvedValue(true)
+    expect(await licenceNotYetAccepted({}, {})).toBeFalsy()
+  })
+
+  test('should return true if not accepted in session and not accepted in DB', async () => {
+    getFromSession.mockReturnValue()
+    validateLicence.mockResolvedValue(false)
+    expect(await licenceNotYetAccepted({}, {})).toBeTruthy()
   })
 })
