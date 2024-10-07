8a4e38c01ed75f29cb81c85b1c89b1af84110104
work <jeremy.barnsley@esynergy.co.uk>
2024-10-07 13:52:28 +0100

    Changed wording of accpet terms Fixed invalid user logout

--- app/schema/licence.js
+++ app/schema/licence.js
@@ -2,7 +2,7 @@ const Joi = require('joi')
 
 const schema = Joi.object({
   accept: Joi.string().required().messages(
-    { '*': 'Tick the box to continue' }
+    { '*': 'You must accept the terms to continue.' }
   )
 }).required()
 
