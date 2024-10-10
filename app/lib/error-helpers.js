287ec38e8e89e24d3ff9904edcecf40cb4a333fa
work <jeremy.barnsley@esynergy.co.uk>
2024-10-10 10:36:21 +0100

    Reworked error pages

--- app/lib/error-helpers.js
+++ app/lib/error-helpers.js
@@ -1,3 +1,5 @@
+const { clearSessionDown } = require('../session/session-wrapper')
+
 const errorPusherDefault = (errors, model) => {
   if (errors) {
     for (const error of errors.details) {
@@ -11,6 +13,41 @@ const errorPusherDefault = (errors, model) => {
     }
   }
 }
+
+const processPreErrorPageResponse = (request, h) => {
+  const response = request.response
+
+  if (response.isBoom) {
+    // An error was raised during
+    // processing the request
+    const statusCode = response.output.statusCode
+
+    console.log('statusCode', statusCode)
+    // if not authorised then request login
+    if (statusCode === 401 || statusCode === 403) {
+      clearSessionDown(request, h)
+      return h.view('unauthorized').code(statusCode)
+    }
+
+    // In the event of 404
+    // return the `404` view
+    if (statusCode === 404) {
+      return h.view('404', { nav: { homeLink: '/' } }).code(statusCode)
+    }
+
+    request.log('error', {
+      statusCode,
+      data: response.data,
+      message: response.message
+    })
+
+    // The return the `500` view
+    return h.view('500', { nav: { homeLink: '/' } }).code(statusCode)
+  }
+  return undefined
+}
+
 module.exports = {
-  errorPusherDefault
+  errorPusherDefault,
+  processPreErrorPageResponse
 }
