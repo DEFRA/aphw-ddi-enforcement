97654633f1eefa007699ec38b429baf3013777d1
Chris Cole <whitewaterdesign>
2024-10-16 17:08:58 +0100

    test: 459655 - Sonar fix

--- app/api/shared.js
+++ app/api/shared.js
@@ -1,5 +1,5 @@
 const { createBearerHeader } = require('../auth/jwt-utils')
-const addHeaders = (user, audience = 'aphw-ddi-api', _request) => {
+const addHeaders = (user, audience = 'aphw-ddi-api') => {
   return {
     'ddi-username': user?.username,
     'ddi-displayname': user?.displayname,
