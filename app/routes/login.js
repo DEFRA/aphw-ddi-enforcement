563aac3ef21555eb814071eef907d2dfe0e08cec
Chris Cole <whitewaterdesign>
2024-10-24 15:30:03 +0100

    Revert "perf: 460652 - Perf testing setup"
    This reverts commit 0198cd308a035c671c47d3626601da04fed1ef2a.

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,3 +1,7 @@
+const auth = require('../auth')
+const { VECTOR_OF_TRUST } = require('../auth/openid-auth')
+const { setInSession } = require('../session/session-wrapper')
+
 module.exports = {
   method: 'GET',
   path: '/login',
@@ -6,8 +10,19 @@ module.exports = {
   },
   handler: async (request, h) => {
     try {
+      setInSession(request, 'returnUrl', request.query?.next)
+
+      if (auth.authType === 'dev') {
+        return h.redirect(auth.getAuthenticationUrl())
+      }
+
+      const authProvider = await auth.getAuth()
+
+      // Calculate the redirect URL the should be returned to after completing the OAuth flow
+      const authorizationUrl = authProvider.getAuthorizationUrl(request, h, authProvider.client, VECTOR_OF_TRUST, undefined, request.query)
+
       // Redirect to the authorization server
-      return h.redirect('/authenticate')
+      return h.redirect(authorizationUrl)
     } catch (err) {
       console.error('Error authenticating:', err)
     }
