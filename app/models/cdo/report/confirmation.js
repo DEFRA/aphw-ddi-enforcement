ce7c40d59c02b1a9bf20343846c5b897cc260ab0
Chris Cole <whitewaterdesign>
2024-11-01 08:17:49 +0000

    fix: 466896 - Fix owner link on Owner-Something Else

--- app/models/cdo/report/confirmation.js
+++ app/models/cdo/report/confirmation.js
@@ -1,10 +1,11 @@
 const { buildReportSubTitle } = require('../../../lib/model-helpers')
+const { reportTypes } = require('./report-type')
 
 function ViewModel (payload) {
   this.model = {
     sourceType: payload?.sourceType,
     pk: payload?.pk,
-    dogIndexOverride: payload?.dogChosen?.indexNumber,
+    dogIndexOverride: payload?.reportType === reportTypes.somethingElse ? undefined : payload?.dogChosen?.indexNumber,
     subTitle: buildReportSubTitle(payload),
     numberOfDogs: payload?.dogs?.length ?? 1
   }
