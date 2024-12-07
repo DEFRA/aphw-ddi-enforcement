e59f0cb2df061be0fc9d5ccf24fdd840858b87f2
Chris Cole <whitewaterdesign>
2024-12-07 21:20:06 +0000

    test: 479627 - Update Cdo test for submitFormTwo

--- test/unit/api/ddi-index-api/cdo.test.js
+++ test/unit/api/ddi-index-api/cdo.test.js
@@ -51,9 +51,33 @@ describe('CDO API endpoints', () => {
 
   describe('saveCdoTaskDetails', () => {
     test('should do POST to API with correct endpoint and payload', async () => {
-      await cdo.saveCdoTaskDetails('ED123', 'submitFormTwo', { payload: 'abc' }, user)
+      await cdo.saveCdoTaskDetails('ED123', 'submitFormTwo', {
+        dogNotFitForMicrochip: false,
+        dogNotNeutered: false,
+        microchipNumber: '123456789012358',
+        'microchipVerification-day': 1,
+        'microchipVerification-month': 10,
+        'microchipVerification-year': 2024,
+        'neuteringConfirmation-day': 1,
+        'neuteringConfirmation-month': 10,
+        'neuteringConfirmation-year': 2024,
+        'microchipDeadline-day': 1,
+        'microchipDeadline-month': 10,
+        'microchipDeadline-year': 2024,
+        microchipDeadline: { year: '2024', month: '10', day: '1' },
+        microchipVerification: { year: '2024', month: '10', day: '1' },
+        neuteringConfirmation: { day: '2024', month: '10', year: '1' },
+        taskName: 'submit-form-two'
+      }, user)
 
-      expect(boomRequest).toHaveBeenCalledWith('cdo/ED123/manage:submitFormTwo', 'POST', { payload: 'abc' }, user)
+      expect(boomRequest).toHaveBeenCalledWith('cdo/ED123/manage:submitFormTwo', 'POST', {
+        dogNotFitForMicrochip: false,
+        dogNotNeutered: false,
+        microchipNumber: '123456789012358',
+        microchipDeadline: { year: '2024', month: '10', day: '1' },
+        microchipVerification: { year: '2024', month: '10', day: '1' },
+        neuteringConfirmation: { day: '2024', month: '10', year: '1' }
+      }, user)
     })
     test('should not POST to API for non-enforcement submittable task', async () => {
       await expect(cdo.saveCdoTaskDetails('ED123', 'send-application-pack', { payload: 'abc' }, user)).rejects.toThrow(new Error('Task may not be saved'))
