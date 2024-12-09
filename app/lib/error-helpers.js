9d9b6af25bcf41563323cf91ddbe04eb93959a22
Chris Cole <whitewaterdesign>
2024-12-09 06:19:14 +0000

    test: 479627 - Sonar Cloud issues 3

--- app/lib/error-helpers.js
+++ app/lib/error-helpers.js
@@ -16,20 +16,23 @@ const errorPusherDefault = (errors, model) => {
 }
 
 const errorPusherWithDate = (errors, model) => {
-  if (errors) {
-    for (const error of errors.details) {
-      let name = error.path[0] ?? error.context.path[0]
-      const prop = model[name]
+  if (!errors) {
+    return
+  }
+  for (const error of errors.details) {
+    let name = error.path[0] ?? error.context.path[0]
+    const prop = model[name]
 
-      if (prop) {
-        if (prop.type === 'date') {
-          name = addDateErrors(error, prop)
-        }
-
-        prop.errorMessage = { text: error.message }
-        model.errors.push({ text: error.message, href: `#${name}` })
-      }
+    if (!prop) {
+      continue
     }
+
+    if (prop.type === 'date') {
+      name = addDateErrors(error, prop)
+    }
+
+    prop.errorMessage = { text: error.message }
+    model.errors.push({ text: error.message, href: `#${name}` })
   }
 }
 
