bba66aa4afe01e4109f4c4895b5f0e7d93464296
Chris Cole <whitewaterdesign>
2024-12-10 14:43:42 +0000

    chore: Remove redundant code

--- app/schema/portal/cdo/tasks/record-verification-dates.js
+++ app/schema/portal/cdo/tasks/record-verification-dates.js
@@ -1,42 +1,7 @@
 const Joi = require('joi')
-const { validateDate, getDateComponents } = require('../../../../lib/date-helpers')
+const { getDateComponents } = require('../../../../lib/date-helpers')
 const { validatePayloadBuilder } = require('../../../../schema/common/validatePayload')
 const { validateMicrochipNumber, validateMicrochipVerification, validateNeuteringConfirmation } = require('../../../../lib/validation-helpers')
-
-const microchipVerification = Joi.object({
-  year: Joi.string().allow(null).allow(''),
-  month: Joi.string().allow(null).allow(''),
-  day: Joi.string().allow(null).allow('')
-}).required().custom((value, helper) => validateDate(value, helper, true, true))
-  .messages({
-    'any.required': 'Enter the date the dog’s microchip number was verified'
-  })
-
-const neuteringConfirmation = Joi.object({
-  year: Joi.string().allow(null).allow(''),
-  month: Joi.string().allow(null).allow(''),
-  day: Joi.string().allow(null).allow('')
-}).required().custom((value, helper) => validateDate(value, helper, true, true))
-  .messages({
-    'any.required': 'Enter the date the dog’s neutering was verified'
-  })
-
-const emptyDate = Joi.object({
-  year: Joi.string().allow('').allow(null),
-  month: Joi.string().allow('').allow(null),
-  day: Joi.string().allow('').allow(null)
-}).custom((value, helpers) => {
-  const keys = ['year', 'month', 'day']
-  const allValid = keys.every(key => value[key] === '')
-
-  if (!allValid) {
-    // Throw a single error if any key has an invalid value
-    const elementPath = helpers.state.path[0]
-    return helpers.message('custom', { path: [elementPath, ['day', 'month', 'year']] })
-  }
-
-  return value
-})
 
 const verificationDatesSchema = Joi.object({
   microchipNumber: Joi.any().custom((value, helper) => validateMicrochipNumber(value, helper)),
@@ -67,8 +32,5 @@ const validateVerificationDates = (payload) => {
 
 module.exports = {
   validateVerificationDates,
-  microchipVerification,
-  neuteringConfirmation,
-  emptyDate,
   verificationDatesSchema
 }
