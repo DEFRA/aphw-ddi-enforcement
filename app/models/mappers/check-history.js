4ebc55fcc3c6b4ea849daf4edc982f021b096b1a
work <jeremy.barnsley@esynergy.co.uk>
2024-12-18 16:21:02 +0000

    Refactor if

--- app/models/mappers/check-history.js
+++ app/models/mappers/check-history.js
@@ -191,21 +191,22 @@ const getActivityLabelFromEvent = (event) => {
     return 'NOT YET DEFINED'
   }
 
-  if (event.activity?.activityType) {
-    // Special case - for Form 2 sent
-    if (event.activity.activityType === 'sent' && event.activity?.activityLabel.startsWith('Form 2 from ')) {
-      return `${event.activity.activityLabel} requested`
-    }
-
-    if (event.activity.activityType === 'received' && event.activity?.activityLabel.toLowerCase().includes('received')) {
-      return `${event.activity.activityLabel}`
-    }
-
-    return `${event.activity?.activityLabel} ${event.activity.activityType}`
+  if (!event.activity?.activityType) {
+    return 'NOT YET DEFINED'
   }
 
-  return 'NOT YET DEFINED'
+  // Special case - for Form 2 sent
+  if (event.activity.activityType === 'sent' && event.activity?.activityLabel?.startsWith('Form 2 from ')) {
+    return `${event.activity.activityLabel} requested`
+  }
+
+  if (event.activity.activityType === 'received' && event.activity?.activityLabel?.toLowerCase().includes('received')) {
+    return `${event.activity.activityLabel}`
+  }
+
+  return `${event.activity?.activityLabel} ${event.activity.activityType}`
 }
+
 /**
  * @typedef ActivityRow
  * @property {string} [activityId]
